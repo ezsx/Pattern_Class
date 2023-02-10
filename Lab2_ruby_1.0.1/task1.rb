ad054e179201442d737c694b416a59f80eae1c08
ezsx <scdcor@gmail.com>
2023-02-10 13:44:17 +0300

    point 8, all validated

--- Lab2_ruby_1.0.1/task1.rb
+++ Lab2_ruby_1.0.1/task1.rb
@@ -10,20 +10,72 @@ class Student
     @telegram = args[:telegram]
     @mail = args[:mail]
     @git = args[:git]
+    validate_fields
+  end
+
+  def validate_fields
+    validate_surname
+    validate_first_name
+    validate_patronymic
     validate_phone_number
+    validate_telegram
+    validate_mail
+    validate_git
+  end
+
+  def validate_surname
+    raise ArgumentError, "Invalid surname" unless self.class.valid_surname?(@surname)
+  end
+
+  def validate_first_name
+    raise ArgumentError, "Invalid first name" unless self.class.valid_first_name?(@first_name)
+  end
+
+  def validate_patronymic
+    raise ArgumentError, "Invalid patronymic" unless self.class.valid_patronymic?(@patronymic)
   end
 
   def validate_phone_number
     raise ArgumentError, "Invalid phone number" unless self.class.valid_phone_number?(@phone)
   end
 
+  def validate_telegram
+    raise ArgumentError, "Invalid telegram username" unless self.class.valid_telegram?(@telegram)
+  end
+
+  def validate_mail
+    raise ArgumentError, "Invalid email address" unless self.class.valid_mail?(@mail)
+  end
+
+  def validate_git
+    raise ArgumentError, "Invalid git username" unless self.class.valid_git?(@git)
+  end
+
+  def self.valid_surname?(surname)
+    surname.is_a?(String) && surname.match(/\A[a-zA-Z]+\z/)
+  end
+
+  def self.valid_first_name?(first_name)
+    first_name.is_a?(String) && first_name.match(/\A[a-zA-Z]+\z/)
+  end
+
+  def self.valid_patronymic?(patronymic)
+    patronymic.is_a?(String) && patronymic.match(/\A[a-zA-Z]+\z/)
+  end
+
   def self.valid_phone_number?(phone)
     phone =~ /\A\d{3}-\d{3}-\d{4}\z/
   end
+
+  def self.valid_telegram?(telegram)
+    telegram.is_a?(String) && telegram.match(/\A@[a-zA-Z0-9]+\z/)
+  end
+
+  def self.valid_mail?(mail)
+    mail.is_a?(String) && mail.match(/\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}\z/)
+  end
+
+  def self.valid_git?(git)
+    git.is_a?(String) && git.match(/\A[a-zA-Z0-9]+\z/)
+  end
 end
-
-student = Student.new({id: 1, surname: 'Johnson', first_name: 'John', patronymic: 'Doe', phone: '555-555-5555'})
-puts student.phone # 555-555-5555
-
-student = Student.new({id: 2, surname: 'Smith', first_name: 'Jane', patronymic: 'Doe', phone: '555-555-555'})
-# Raises ArgumentError: Invalid phone number
