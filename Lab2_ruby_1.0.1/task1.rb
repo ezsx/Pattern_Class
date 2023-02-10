610957ad816b3ab9cbd6bf4a0f74b8eb5cef9229
ezsx <scdcor@gmail.com>
2023-02-10 13:40:29 +0300

    point 7, phone

--- Lab2_ruby_1.0.1/task1.rb
+++ Lab2_ruby_1.0.1/task1.rb
@@ -10,5 +10,20 @@ class Student
     @telegram = args[:telegram]
     @mail = args[:mail]
     @git = args[:git]
+    validate_phone_number
+  end
+
+  def validate_phone_number
+    raise ArgumentError, "Invalid phone number" unless self.class.valid_phone_number?(@phone)
+  end
+
+  def self.valid_phone_number?(phone)
+    phone =~ /\A\d{3}-\d{3}-\d{4}\z/
   end
 end
+
+student = Student.new({id: 1, surname: 'Johnson', first_name: 'John', patronymic: 'Doe', phone: '555-555-5555'})
+puts student.phone # 555-555-5555
+
+student = Student.new({id: 2, surname: 'Smith', first_name: 'Jane', patronymic: 'Doe', phone: '555-555-555'})
+# Raises ArgumentError: Invalid phone number
