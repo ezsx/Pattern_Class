625c97f6ccd5b5d95e78a67916e27d8f9b457a92
ezsx <scdcor@gmail.com>
2023-02-10 13:48:16 +0300

    point 9, validate_guide validate_contact

--- Lab2_ruby_1.0.1/task1.rb
+++ Lab2_ruby_1.0.1/task1.rb
@@ -1,15 +1,15 @@
 class Student
   attr_accessor :id, :surname, :first_name, :patronymic, :phone, :telegram, :mail, :git
 
-  def initialize(args = {})
-    @id = args[:id]
-    @surname = args[:surname]
-    @first_name = args[:first_name]
-    @patronymic = args[:patronymic]
-    @phone = args[:phone]
-    @telegram = args[:telegram]
-    @mail = args[:mail]
-    @git = args[:git]
+  def initialize(options = {})
+    @id = options[:id]
+    @surname = options[:surname]
+    @first_name = options[:first_name]
+    @patronymic = options[:patronymic]
+    @phone = options[:phone]
+    @telegram = options[:telegram]
+    @mail = options[:mail]
+    @git = options[:git]
     validate_fields
   end
 
@@ -17,38 +17,28 @@ class Student
     validate_surname
     validate_first_name
     validate_patronymic
-    validate_phone_number
-    validate_telegram
-    validate_mail
-    validate_git
+    validate_guide
+    validate_contact
   end
 
   def validate_surname
-    raise ArgumentError, "Invalid surname" unless self.class.valid_surname?(@surname)
+    raise ArgumentError, 'Surname is required' unless self.class.valid_surname?(surname)
   end
 
   def validate_first_name
-    raise ArgumentError, "Invalid first name" unless self.class.valid_first_name?(@first_name)
+    raise ArgumentError, 'First name is required' unless self.class.valid_first_name?(first_name)
   end
 
   def validate_patronymic
-    raise ArgumentError, "Invalid patronymic" unless self.class.valid_patronymic?(@patronymic)
+    raise ArgumentError, 'Patronymic is required' unless self.class.valid_patronymic?(patronymic)
   end
 
-  def validate_phone_number
-    raise ArgumentError, "Invalid phone number" unless self.class.valid_phone_number?(@phone)
+  def validate_guide
+    raise ArgumentError, 'A guide is required' unless (git || telegram || mail)
   end
 
-  def validate_telegram
-    raise ArgumentError, "Invalid telegram username" unless self.class.valid_telegram?(@telegram)
-  end
-
-  def validate_mail
-    raise ArgumentError, "Invalid email address" unless self.class.valid_mail?(@mail)
-  end
-
-  def validate_git
-    raise ArgumentError, "Invalid git username" unless self.class.valid_git?(@git)
+  def validate_contact
+    raise ArgumentError, 'A contact for communication is required' unless (phone || telegram || mail)
   end
 
   def self.valid_surname?(surname)
@@ -63,19 +53,19 @@ class Student
     patronymic.is_a?(String) && patronymic.match(/\A[a-zA-Z]+\z/)
   end
 
-  def self.valid_phone_number?(phone)
-    phone =~ /\A\d{3}-\d{3}-\d{4}\z/
+  def self.valid_phone?(phone)
+    phone.is_a?(String) && phone.match(/\A[0-9]+\z/)
   end
 
   def self.valid_telegram?(telegram)
-    telegram.is_a?(String) && telegram.match(/\A@[a-zA-Z0-9]+\z/)
+    telegram.is_a?(String) && telegram.match(/\A[a-zA-Z0-9]+\z/)
   end
 
   def self.valid_mail?(mail)
-    mail.is_a?(String) && mail.match(/\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}\z/)
+    mail.is_a?(String) && mail.match(/\A[a-zA-Z0--9]+@[a-z]+.[a-z]+\z/)
   end
 
   def self.valid_git?(git)
-    git.is_a?(String) && git.match(/\A[a-zA-Z0-9]+\z/)
+    git.is_a?(String) && git.match(/\Ahttps?://github.com/[a-zA-Z0-9]+/[a-zA-Z0-9]+\z/)
   end
 end
