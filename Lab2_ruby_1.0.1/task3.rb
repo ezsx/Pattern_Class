d9dfaabb63d97e31ee18a72e3096d338c652aadf
ezsx <scdcor@gmail.com>
2023-02-12 16:27:00 +0300

    part3 task6,7

--- Lab2_ruby_1.0.1/task3.rb
+++ Lab2_ruby_1.0.1/task3.rb
@@ -54,3 +54,61 @@ class DataList
   private
   attr_reader :data
 end
+
+# class DataListStudentShort < DataList
+#   # realisation of get_names withot using each method cant be done
+#   # because of the realization of DataTabel class
+#
+#   def get_data
+#     data = []
+#     each do |student|
+#       data << [student.id, student.surname, student.initials, student.git, student.contact]
+#     end
+#     DataTable.new(data)
+#   end
+#
+#   def get_names
+#     ['ID', 'Surname', 'Initials', 'Git', 'Contact']
+#   end
+# end
+class DataListStudentShort < DataList
+  attr_reader :data_table
+
+  private
+
+  def parse_student_short(student_short)
+    [student_short.id, student_short.surname, student_short.initials, student_short.git, student_short.contact]
+  end
+
+  public
+
+  def get_data
+    DataTable.new(
+          elements: elements.map { |student_short| parse_student_short(student_short) }
+        )
+  end
+
+  def get_names
+    %w[id surname initials git contact]
+  end
+end
+
+# the simple patern template example
+#
+# def parse_pattern(input)
+#   case input
+#   when "hello"
+#     puts "You said hello!"
+#   when "goodbye"
+#     puts "You said goodbye!"
+#   when /\A\d+\z/
+#     puts "You entered a number."
+#   else
+#     puts "I don't understand what you said."
+#   end
+# end
+#
+# parse_pattern("hello") # outputs: You said hello!
+# parse_pattern("goodbye") # outputs: You said goodbye!
+# parse_pattern("123") # outputs: You entered a number.
+# parse_pattern("abc") # outputs: I don't understand what you said.
