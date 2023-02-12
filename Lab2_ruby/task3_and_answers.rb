5fe44f6eae85450716f146853c2513ccd0f17e0d
ezsx <scdcor@gmail.com>
2023-02-12 20:38:33 +0300

    part3 all complete, and tested, but may be not work, xz chestno vse full krinzh i think

--- Lab2_ruby/task3_and_answers.rb
+++ Lab2_ruby/task3_and_answers.rb
@@ -1,13 +1,16 @@
-require_relative 'task1_2'
+require_relative ''
+
+# make relative path to the file in another directory
 
 
 class Data_table
   attr_accessor :data
 
-  def initialize(params ={data: []})
+  def initialize(params = { data: [] })
     # Initialize the two-dimensional array with elements of any type
     @data = params[:data]
   end
+
   # hash = {a: 1, b: 2, c: 3}
   def to_h
     headers = data.first
@@ -52,15 +55,27 @@ class Data_list
 
   def get_names
     # Get an array of attribute names, except ID (0 columns - No. in order)
+    attributes = []
+    @data.each do |element|
+      attributes << element.get_names
+    end
     raise NotImplementedError, "This method should be implemented in the inheritors."
   end
 
   def get_data(selected = nil)
     # Get an object of the Data_table class, where the null column is the generated number in order
     # and the first column is the ID of the selected element
-
-
+    data = Data_table.new
+    data.data << get_names
+    @data.each_with_index do |element, index|
+      data.data << [index, element.id] if selected == nil || selected.include?(index)
+    end
     raise NotImplementedError, "This method should be implemented in the inheritors."
+  end
+  def selected?
+    # Check if the element is selected
+
+
   end
 end
 
@@ -70,9 +85,29 @@ class Data_list_student_short < Data_list
     [:id, :surname, :initials, :git, :contact]
   end
 
+  # setter
+  def create_data_table
+    data = []
+    @data.each_with_index do |element, index|
+      data << [index, element.id, element.surname, element.initials, element.git, element.contact]
+    end
+    data
+  end
+
+  def create_names
+    names = []
+    names << 'No.'
+    names << 'ID'
+    names << 'Surname'
+    names << 'Initials'
+    names << 'Git'
+    names << 'Contact'
+    names
+  end
+
   def get_data
     # Override the get_data method to get an object of the Data_table class for the class Student_short
-    data = Data_table.new(create_data_table, create_names)
+    data = Data_table.new(create_data_table)
     data
   end
 end
